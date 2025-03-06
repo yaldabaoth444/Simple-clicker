af7cde63abb61f2d6bdae9728373ca7893297480
Azamat <g.azamat@unibell.ru>
2025-03-07 02:26:02 +0500

    ssyntax

--- clicker.js
+++ clicker.js
@@ -1,3 +1,24 @@
+// The MIT License
+//
+// Copyright © 2023 Gizatullin Azamat (monopo@list.ru)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the “Software”), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to
+// whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+// THE SOFTWARE.
+
 const core = require('./minimal-core.js');
 
 "auto";
@@ -12,73 +33,75 @@ var lastTemplateTime = null;
 var imgfRes = null;
 
 //IMPORTANT POINT (infinte loop)
-while(true) {
-	
-	//IMPORTANT POINT (find template match)
-	let imgad = captureScreen();
-	imgfRes = core.ImagesFinderEx(imgad, templates);
-	if (imgad != null)
+while (true) {
+
+    //IMPORTANT POINT (find template match)
+    let imgad = captureScreen();
+    imgfRes = core.ImagesFinderEx(imgad, templates);
+    if (imgad != null)
         imgad.recycle();
 
     //if found
-	if (imgfRes.result) {
-		let pos = imgfRes.position_center;
-		//parse additional options
-		let link = core.GetLink(imgfRes.image_name);
-		let tout = core.GetTimeout(imgfRes.image_name);
-		let tin = core.GetTimein(imgfRes.image_name);
-		let customXY = core.GetXYCoord(imgfRes.image_name);
-		
-		if (customXY) 
-			pos = customXY;
-		
-		if (lastTemplateName != imgfRes.image_name) {
-			core.StartTimer('last');
-			lastTemplateName = imgfRes.image_name;
-		}
+    if (imgfRes.result) {
+        let pos = imgfRes.position_center;
+        //parse additional options
+        let link = core.GetLink(imgfRes.image_name);
+        let tout = core.GetTimeout(imgfRes.image_name);
+        let tin = core.GetTimein(imgfRes.image_name);
+        let customXY = core.GetXYCoord(imgfRes.image_name);
 
-		//we wait until the image exists for a set number of milliseconds then click it
-		if (tin && core.Elapsed('last') < tin) 
-			continue;
+        if (customXY)
+            pos = customXY;
+
+        if (lastTemplateName != imgfRes.image_name) {
+            core.StartTimer('last');
+            lastTemplateName = imgfRes.image_name;
+        }
+
+        //we wait until the image exists for a set number of milliseconds then click it
+        if (tin && core.Elapsed('last') < tin)
+            continue;
 
         //IMPORTANT POINT (click)
-		core.Press(pos, 100);
-		log(imgfRes.image_name + " [" + pos.x + "," + pos.y + "]");
+        core.Press(pos, 100);
+        log(imgfRes.image_name + " [" + pos.x + "," + pos.y + "]");
 
-		//pause after pressing for a set number of milliseconds
-		if (tout)
-			sleep(tout); 
+        //pause after pressing for a set number of milliseconds
+        if (tout)
+            sleep(tout);
 
-		//if folder change is specified
-		if (link !== null) {
-			if (link=='exit') break;
-			log("switch to " + link);
-			templates = loadTemplate(startImageFolder, link);
-		}
-		//affects performance		
-		sleep(500); 		
-	}
-	//affects performance
-	sleep(30); 
+        //if folder change is specified
+        if (link !== null) {
+            if (link == 'exit') break;
+            log("switch to " + link);
+            templates = loadTemplate(startImageFolder, link);
+        }
+        //affects performance		
+        sleep(500);
+    }
+    //affects performance
+    sleep(30);
 }
 toastLog("END");
 
-function loadTemplate(folder, subFolder)
-{
-	let sFolder = (subFolder && subFolder.length > 0) ? subFolder+'/':'';
-	let tFolder = './Images/'+folder+'/'+sFolder;
-	var res = [];
+function loadTemplate(folder, subFolder) {
+    let sFolder = (subFolder && subFolder.length > 0) ? subFolder + '/' : '';
+    let tFolder = './Images/' + folder + '/' + sFolder;
+    var res = [];
 
     let tList = core.GetFolderImages(tFolder);
-	tList.sort();
-	if(tList.length > 0){
-	    for(let i = 0; i < tList.length; i++){
-	        let fileName = tList[i];
-	    	let path = files.join(tFolder, fileName);
-	        let img = images.read(path);
-	        let item = {"img": img, "fileName": fileName};
-	        res.push(item);
-	    }
-	}
-	return res;
+    tList.sort();
+    if (tList.length > 0) {
+        for (let i = 0; i < tList.length; i++) {
+            let fileName = tList[i];
+            let path = files.join(tFolder, fileName);
+            let img = images.read(path);
+            let item = {
+                "img": img,
+                "fileName": fileName
+            };
+            res.push(item);
+        }
+    }
+    return res;
 }
--- minimal-core.js
+++ minimal-core.js
@@ -24,100 +24,105 @@ var _core = {};
 _core.timers = storages.create('timers');
 _core.threshold = 0.90;
 
-_core.Click = function(point) {
-	clickPixel(point);
+_core.Click = function (point) {
+    clickPixel(point);
 }
 
-_core.Press = function(point, duration) {
+_core.Press = function (point, duration) {
     press(point.x, point.y, duration)
 }
 
-_core.ImageFinderEx = function(img, template, region)
-{
+_core.ImageFinderEx = function (img, template, region) {
     return imageFinderEx(img, template, region);
 };
 
-_core.ImagesFinder = function(img, folder, region)
-{
-	return imagesFinder(img, folder, region);
+_core.ImagesFinder = function (img, folder, region) {
+    return imagesFinder(img, folder, region);
 };
 
-_core.ImagesFinderEx = function(img, template, region)
-{
+_core.ImagesFinderEx = function (img, template, region) {
     return imagesFinderEx(img, template, region);
 };
 
-_core.GetFolderImages = function(folder) {
+_core.GetFolderImages = function (folder) {
     return getFolderImages(folder);
 };
 
-_core.StartTimer = function(timerName) {
+_core.StartTimer = function (timerName) {
     startTimer(timerName);
 }
 
-_core.ElapsedSeconds = function(timerName) {
+_core.ElapsedSeconds = function (timerName) {
     return elapsedSeconds(timerName);
 }
 
-_core.Elapsed = function(timerName) {
+_core.Elapsed = function (timerName) {
     return elapsed(timerName);
 }
 
-_core.GetScriptName = function() {
+_core.GetScriptName = function () {
     return getScriptName();
 }
 
-_core.GetLink = function(filename) {
+_core.GetLink = function (filename) {
     return getLink(filename);
 }
 
-_core.GetPercent = function(filename) {
+_core.GetPercent = function (filename) {
     return getPercent(filename);
 }
 
-_core.GetTimeout = function(filename) {
+_core.GetTimeout = function (filename) {
     return getTimeout(filename);
 }
 
-_core.GetTimein = function(filename) {
+_core.GetTimein = function (filename) {
     return getTimein(filename);
 }
 
-_core.GetXYCoord = function(filename) {
+_core.GetXYCoord = function (filename) {
     return getXYCoord(filename);
 }
 
 module.exports = _core;
 
-function imagesFinderEx(img, templates, region)
-{
-    let res = { result: false, position: null, position_center: null, image_name: null };
+function imagesFinderEx(img, templates, region) {
+    let res = {
+        result: false,
+        position: null,
+        position_center: null,
+        image_name: null
+    };
 
     if (!templates || templates.length == 0)
         return res;
 
-    for(let i = 0; i < templates.length; i++){
+    for (let i = 0; i < templates.length; i++) {
         let prc = getPercent(templates[i].fileName);
 
         if (prc && prc > 0 && prc < 100) {
-            let rand = getRandomArbitrary(0,100)
+            let rand = getRandomArbitrary(0, 100)
             if (rand > prc) {
                 continue;
             }
         }
-        
+
         res = imageFinderEx(img, templates[i], region);
-        if(res.result){
+        if (res.result) {
             return res
-        }  
+        }
     }
-    
+
     return res
 };
 
-function imageFinderEx(img, template, region)
-{
-    let res = { result: false, position: null, position_center: null, image_name: null };
+function imageFinderEx(img, template, region) {
+    let res = {
+        result: false,
+        position: null,
+        position_center: null,
+        image_name: null
+    };
 
     if (!template)
         return res;
@@ -127,115 +132,116 @@ function imageFinderEx(img, template, region)
     let pos = null;
     if (_region) {
         if (typeof _region === 'string')
-          _region = calcRegion(img, _region)
+            _region = calcRegion(img, _region)
 
         pos = images.findImageInRegion(img, tImg, _region.x, _region.y, _region.width, _region.height, _core.threshold);
 
         if (!insideRegion(_region, pos))
             return res;
-    }
-    else
-        pos = images.findImage(img, tImg, {threshold: _core.threshold});    
+    } else
+        pos = images.findImage(img, tImg, {
+            threshold: _core.threshold
+        });
 
     width = tImg.getWidth();
     height = tImg.getHeight();
-    
-    if(pos){
+
+    if (pos) {
         res.result = true;
         res.position = pos;
         res.position_center = {
-            x: Math.round(pos.x + width/2), 
-            y: Math.round(pos.y + height/2)
+            x: Math.round(pos.x + width / 2),
+            y: Math.round(pos.y + height / 2)
         };
         res.image_name = template.fileName;
-    } 
+    }
     return res;
 }
 
-function getFolderImages(folder) 
-{
-    return files.listDir(folder, function(name){
+function getFolderImages(folder) {
+    return files.listDir(folder, function (name) {
         return (name.endsWith(".png") || name.endsWith(".jpg")) && files.isFile(files.join(folder, name));
     });
 }
 
-function getLocation(filename)
-{
+function getLocation(filename) {
     const regex = /@[tb]?[rl]?\d+/gi;
     const regex2 = /@[tbrl]{1,2}\d{1,2}/gi;
     var matches = filename.match(regex);
-    if (matches){
-      matches = matches[0].match(regex2);
-      if (matches)
-        return matches[0].substring(1);
+    if (matches) {
+        matches = matches[0].match(regex2);
+        if (matches)
+            return matches[0].substring(1);
     }
     return null;
 }
 
-function getTolerance(filename)
-{
+function getTolerance(filename) {
     const regex = /~\d{2}/gi;
     var matches = filename.match(regex);
-    if (matches){
+    if (matches) {
         return parseFloat("0." + matches[0].substring(1));
     }
     return null;
 }
 
-function getPercent(filename)
-{
+function getPercent(filename) {
     const regex = /%\d{2}/gi;
     var matches = filename.match(regex);
-    if (matches){
+    if (matches) {
         return parseInt(matches[0].substring(1));
     }
     return null;
 }
 
-function getTimeout(filename)
-{
+function getTimeout(filename) {
     const regex = /»\d+/gi;
     var matches = filename.match(regex);
-    if (matches){
+    if (matches) {
         return parseInt(matches[0].substring(1));
     }
     return null;
 }
 
-function getTimein(filename)
-{
+function getTimein(filename) {
     const regex = /«\d+/gi;
     var matches = filename.match(regex);
-    if (matches){
+    if (matches) {
         return parseInt(matches[0].substring(1));
     }
     return null;
 }
 
-function getXYCoord(filename)
-{
+function getXYCoord(filename) {
     const regex = /{\d+,\d+}/gi;
     var matches = filename.match(regex);
-    if (matches){
-        let res = matches[0].slice(1,-1).split(",");
-        let pos = { x: parseInt(res[0]), y: parseInt(res[1]) };
+    if (matches) {
+        let res = matches[0].slice(1, -1).split(",");
+        let pos = {
+            x: parseInt(res[0]),
+            y: parseInt(res[1])
+        };
         return pos;
     }
     return null;
 }
 
-function getLink(filename)
-{
+function getLink(filename) {
     const regex = /\^\w*/gi;
     var matches = filename.match(regex);
-    if (matches){
+    if (matches) {
         return matches[0].substring(1);
     }
     return null;
 }
 
 function calcRegion(img, location) {
-    let res = { x: 0, y: 0, width: img.width - 1, height: img.height - 1 };
+    let res = {
+        x: 0,
+        y: 0,
+        width: img.width - 1,
+        height: img.height - 1
+    };
 
     let locPercent = parseInt(location.match(/\d+/));
     let xp = img.width * locPercent / 100;
@@ -250,11 +256,11 @@ function calcRegion(img, location) {
         res.height = yp - 1;
     }
 
-    if (location.indexOf("l") !== -1){
+    if (location.indexOf("l") !== -1) {
         res.width = xp;
     }
 
-    if (location.indexOf("r") !== -1){
+    if (location.indexOf("r") !== -1) {
         res.x = img.width - xp;
         res.width = xp - 1;
     }
@@ -268,11 +274,11 @@ function insideRegion(region, pos) {
     if (pos.x >= region.x && pos.x <= region.x + region.width)
         if (pos.y >= region.y && pos.y <= region.y + region.height)
             return true;
-    return false;    
+    return false;
 }
 
 function clickPixel(point) {
-	click(point.x, point.y);
+    click(point.x, point.y);
 }
 
 function startTimer(timerName) {
@@ -293,7 +299,7 @@ function elapsed(timerName) {
 }
 
 function getScriptName() {
-    return getStringBetween(''+engines.myEngine().source, "/", ".js");
+    return getStringBetween('' + engines.myEngine().source, "/", ".js");
 }
 
 function getStringBetween(text, first, last) {
@@ -304,6 +310,6 @@ function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-Array.prototype.contains = function(element){
+Array.prototype.contains = function (element) {
     return this.indexOf(element) > -1;
 };
